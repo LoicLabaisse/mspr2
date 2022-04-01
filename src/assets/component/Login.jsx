d0233ce28752428f09a3bbba8e87ccce254392ce
Charlotte-Meniere <charlotte.meniere@orange.fr>
2022-04-01 16:07:27 +0200

    archi

--- src/assets/component/Login.jsx
+++ src/assets/component/Login.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import React, { useState } from "react";
-import "./Login.css";
+import "../css/Login.css";
 
 const Login = () => {
   const [login, setLogin] = useState({
