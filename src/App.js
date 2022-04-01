45cfda12fdfa25e8d1eeb19af40493a3f35d4012
Charlotte-Meniere <charlotte.meniere@orange.fr>
2022-04-01 17:00:06 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './component/Login';
-import ListePatient from './component/ListePatient';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./assets/component/Login";
+import ListePatient from "./assets/component/ListePatient";
 
 function App() {
   return (
     <div className="App">
       <Router>
-          <Routes>
-            <Route path='/' element={<Login/>}/>
-            <Route path='/listepatient' element={<ListePatient/>}/>
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/listepatient" element={<ListePatient />} />
+        </Routes>
       </Router>
     </div>
   );
